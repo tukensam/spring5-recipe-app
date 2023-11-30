6805a70075b4f5513a586c7ef3d018fe98f226ca
Sam Murk Caya <samuel_murk_caya@alumni.brown.edu>
2023-11-30 10:06:56 -0500

    Updated POM, index.html, and recipe controller

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -21,7 +21,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
